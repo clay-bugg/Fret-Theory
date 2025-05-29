13e5dff4ed2132fc73b2d8c342e724dbfe01d52b
clay-bugg <kalebutler2525@gmail.com>
2025-05-29 02:05:24 +0100

    installed Nuxt Icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/icon",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          "Inter": [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        },
+      },
+    ],
+  ],
+});
