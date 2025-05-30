9ec709a8106e9d94588b829574a14551974d3b33
clay-bugg <kalebutler2525@gmail.com>
2025-05-30 12:28:55 +0100

    added chord selector inputs to control panel

--- components/ChordSelector.vue
+++ components/ChordSelector.vue
@@ -1,9 +1,29 @@
 <template>
-  
+  <div class="control">
+    <p>Chord Finder</p>
+    <div class="chord-finder-options">
+
+      <select  v-model="rootNote"  class="note-selector">
+        <option v-for="note in keysStore.notes" :key="note" :value="note">
+          {{ note.replace('s', '#') }}
+        </option>
+      </select>
+
+      <select v-model="chordType" class="chord-selector">
+        <option v-for="chord in chordStore.chordTypes"
+          :key="chord.value"
+          :value="chord.value">
+            {{ chord.label }}
+        </option>
+      </select>
+
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+const chordStore = useChordTypeStore();
+const keysStore = useKeyboardStore();
 </script>
 
 <style scoped>
